ff184cb2616e9e63ab51e9b3a29a659b399f3b62
thu <mingfei.net@gmail.com>
2016-06-21 16:09:51 +0800

    Update.

---
+++ src/ForTest.java
@@ -0,0 +1,11 @@
+/**
+ * Created by mingfei.net@gmail.com
+ * 2016/6/21 16:02
+ */
+public class ForTest {
+    public static void main(String[] args) {
+        for (int i = 0; i <100; i++) {
+            System.out.println(i);
+        }
+    }
+}

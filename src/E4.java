5e132f0ecf2e694f5795135b391f1648980b1285
thu <mingfei.net@gmail.com>
2016-06-22 13:27:57 +0800

    Update.

---
+++ src/E4.java
@@ -0,0 +1,14 @@
+/**
+ * Created by mingfei.net@gmail.com
+ * 2016/6/22 13:22
+ */
+// 将一个正整数分解质因数。
+// 例如：输入90，打印出90 = 2*3*3*5。
+public class E4 {
+    public static void main(String[] args) {
+        // 100 = 2*2*5*5
+        // 120 = 2*2*2*3*5
+        // 1000 = 2*2*2*5*5*5
+        // 123456789 =
+    }
+}

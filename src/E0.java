87a3ae6cf86aaa3e327fea945b3e8d3f660cfb23
thu <mingfei.net@gmail.com>
2016-06-22 15:46:17 +0800

    Update.

--- src/E0.java
+++ src/E0.java
@@ -2,17 +2,21 @@
  * Created by mingfei.net@gmail.com
  * 2016/6/22 14:37
  */
-// 1, 1, 2, 3, 5, 8, 13, 21, 34, 55...
-// F(n) = ?
+// 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89...
+// F(n) = 1 (n=1,n=2)
+// F(n) = F(n-2) + F(n-1) (n>2)
 public class E0 {
     public static void main(String[] args) {
 
-        int n = 10;
-        System.out.println(compute(n));
+        int n = 51;
+        System.out.println("f(" + n + ") = " + f(n));
     }
 
-    static int compute(int n) {
-
-        return 0;
+    static long f(int n) { // fibonacci
+        if (n == 1 || n == 2) {
+            return 1;
+        } else {
+            return f(n - 2) + f(n - 1);
+        }
     }
 }
---
+++ src/E9.java
@@ -0,0 +1,7 @@
+/**
+ * Created by mingfei.net@gmail.com
+ * 2016/6/22 15:25
+ */
+// 9 10 13 14 15 16 19 20 21 22
+public class E9 {
+}

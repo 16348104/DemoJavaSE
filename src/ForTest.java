0c49b41109fe5db9ea6b250799a3c45c9669d5c1
thu <mingfei.net@gmail.com>
2016-06-22 09:31:24 +0800

    Update.

--- src/ForTest.java
+++ src/ForTest.java
@@ -5,12 +5,26 @@
 public class ForTest {
     public static void main(String[] args) {
 //        for (int i = 0; i < 10; i++) {
+//            if (i == 5) {
+//                continue;
+//            }
 //            System.out.println(i);
 //        }
-        int i = 0;
-        while (i < 10) {
-            System.out.println(i);
-            i++;
+//        int i = 0;
+//        while (i < 10) {
+//            System.out.println(i);
+//            i++;
+//        }
+
+
+        for (int j = 0; j < 10; j++) {
+            for (int k = 0; k < 10; k++) {
+                if (j == 5) {
+                    break;
+                }
+                System.out.print(j + ", " + k + "\t");
+            }
+            System.out.println();
         }
     }
 }
--- src/WhileTest.java
+++ src/WhileTest.java
@@ -4,10 +4,16 @@
  */
 public class WhileTest {
     public static void main(String[] args) {
-        int i = 0;
-        while (i < 10) {
+ /*       int i = 0;
+        while (i > 10) {
             System.out.println(i);
             i++;
-        }
+        }*/
+
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i > 10);
     }
 }

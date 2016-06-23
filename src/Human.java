e46ca051bcb2eedf23446c1bdd6688d9fc9f681d
thu <mingfei.net@gmail.com>
2016-06-23 13:52:45 +0800

    Update.

--- src/Human.java
+++ src/Human.java
@@ -9,11 +9,26 @@ public class Human {
     int age;
     double height;
     double weight;
+    boolean isMarried;
+
+//    public Human() { // 构造方法 constructor
+//
+//    }
+
+    // Alert + Insert
+
+
+    public Human(String name, String gender, int age, double height, double weight, boolean isMarried) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.isMarried = isMarried;
+    }
 
     public void eat(String food, String time) {
-        System.out.println("eat " + food + ", at " + time);
-        work();
-//        return;
+        System.out.println(name + " eat " + food + ", at " + time);
     }
 
     public int sleep(int hour) {
@@ -29,5 +44,17 @@ public class Human {
 
     public void work() {
 
+    }
+
+    public static void main(String[] args) {
+        Human zhangsan = new Human("张三", "男", 18, 1.7, 65, false); // Ctrl + P(arameter)
+        System.out.println(zhangsan.name);
+        System.out.println(zhangsan.gender);
+        System.out.println(zhangsan.age);
+        System.out.println(zhangsan.height);
+        System.out.println(zhangsan.weight);
+        System.out.println(zhangsan.isMarried);
+
+        zhangsan.eat("米饭", "中午");
     }
 }

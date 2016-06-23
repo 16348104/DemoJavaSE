49131940e8747fda6a440ff6cf2b3dac7fc293fe
thu <mingfei.net@gmail.com>
2016-06-23 16:32:22 +0800

    Update.

--- src/E10.java
+++ src/E10.java
@@ -13,27 +13,25 @@ public class E10 {
 
     double totalDistance(int n) {
         double sum = 0;
-//        // 2
-//        sum = reboundHeight(1) * 2;
-//        // 3
-//        sum = reboundHeight(1) * 2 + reboundHeight(2)*2;
-
-        for (int i = ; i < n; i++) {
-            sum += reboundHeight(n-1) * 2;
+        for (int i = 1; i < n + 1; i++) {
+            sum += reboundHeight(i - 1) * 2;
         }
-
         return height + sum;
     }
 
     double reboundHeight(int n) {
-        return height / Math.pow(2, n);
+        if (n == 0) {
+            return 0;
+        } else {
+            return height / Math.pow(2, n);
+        }
     }
 }
 
 class E10Test {
     public static void main(String[] args) {
         E10 e10 = new E10(100);
-        int n = 2;
+        int n = 100;
         System.out.println("total distance: " + e10.totalDistance(n));
         System.out.println("rebound height of " + n + ": " + e10.reboundHeight(n));
     }

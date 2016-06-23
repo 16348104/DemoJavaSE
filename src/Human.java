730b51c499b3e8f7eeaca3b1972925a1f3e4a81a
thu <mingfei.net@gmail.com>
2016-06-23 09:51:48 +0800

    Update.

---
+++ src/Human.java
@@ -0,0 +1,28 @@
+/**
+ * Created by mingfei.net@gmail.com
+ * 2016/6/23 9:29
+ */
+// class = field(s) + method(s)
+public class Human {
+    String name;
+    String gender;
+    int age;
+    double height;
+    double weight;
+
+    public void eat() {
+
+    }
+
+    public void sleep() {
+
+    }
+
+    public void study() {
+
+    }
+
+    public void work() {
+
+    }
+}

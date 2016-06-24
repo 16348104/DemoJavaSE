70644f575486c39f5871ea71047ff0d4b16359b7
thu <mingfei.net@gmail.com>
2016-06-24 09:30:35 +0800

    Update.

---
+++ src/SuperClass.java
@@ -0,0 +1,17 @@
+/**
+ * Created by mingfei.net@gmail.com
+ * 2016/6/24 9:17
+ */
+public class SuperClass {
+    boolean b;
+    int i;
+    double d;
+
+    void m1() {
+
+    }
+
+    void m2() {
+
+    }
+}

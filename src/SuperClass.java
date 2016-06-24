22003d7c46e42f1307a79b25fd45efb52c8fc82a
thu <mingfei.net@gmail.com>
2016-06-24 10:08:01 +0800

    Update.

--- src/SuperClass.java
+++ src/SuperClass.java
@@ -7,11 +7,64 @@ public class SuperClass {
     int i;
     double d;
 
+//    public SuperClass() {
+//
+//    }
+
+    public SuperClass(int n) {
+        System.out.println("invoke SuperClass constructor...");
+    }
+
     void m1() {
 
     }
 
     void m2() {
 
+    }
+}
+
+class SubClass extends SuperClass {
+
+    public SubClass(int n) {
+        super(n);
+        System.out.println("invoke SubClass constructor...");
+    }
+
+    int j;
+
+    void m3() {
+        super.m1();
+    }
+}
+
+class GrandsonClass extends SubClass {
+    int k;
+
+    public GrandsonClass(int n) {
+        super(n);
+    }
+
+    void m4() {
+
+    }
+}
+
+class SubClassTest {
+    public static void main(String[] args) {
+//        GrandsonClass grandsonClass = new GrandsonClass();
+//        System.out.println(grandsonClass.i);
+//        System.out.println(grandsonClass.j);
+//        System.out.println(grandsonClass.k);
+
+
+//        SuperClass superClass = new SubClass();
+//        System.out.println(superClass.i);
+//        superClass.m1();
+
+//        SubClass subClass = new SubClass();
+//        System.out.println(subClass.j);
+//        subClass.m2();
+//        subClass.m3();
     }
 }

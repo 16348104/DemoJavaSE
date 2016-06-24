6b34c9372ea540056ba428d03fb46f04a67031ce
thu <mingfei.net@gmail.com>
2016-06-24 11:22:22 +0800

    Update.

---
+++ src/FatherClass.java
@@ -0,0 +1,17 @@
+/**
+ * Created by mingfei.net@gmail.com
+ * 2016/6/24 10:33
+ */
+public class FatherClass {
+    int i;
+    double d;
+
+    public FatherClass(int i, double d) {
+        this.i = i;
+        this.d = d;
+    }
+
+    void m1() {
+
+    }
+}

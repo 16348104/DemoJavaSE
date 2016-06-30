48d53f2e9ee212cb373b341ba8f5da626e18804a
thu <mingfei.net@gmail.com>
2016-06-30 16:54:07 +0800

    Update.

--- src/cn/edu/tsinghua/training/javase/basic/ArrayTest.java
+++ src/cn/edu/tsinghua/training/javase/basic/ArrayTest.java
@@ -6,13 +6,17 @@ package cn.edu.tsinghua.training.javase.basic;
  */
 public class ArrayTest {
     public static void main(String[] args) {
-        int[] ints = new int[10];
+        int[] ints = new int[100];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < ints.length; i++) {
             ints[i] = i;
         }
         System.out.println(ints[9]); // index 索引 下标
 //        int ints[] = new int[10];
         System.out.println(ints.length);
+
+        String[] strings = {"test", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hi", "hello", "hitest"};
+        System.out.println(strings.length);
+        System.out.println(strings[strings.length - 1]);
     }
 }

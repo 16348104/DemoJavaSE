b0c5ac3281f527809b32d2b4836b7349041ff752
thu <mingfei.net@gmail.com>
2016-07-04 10:17:46 +0800

    Update.

--- src/cn/edu/tsinghua/training/javase/basic/StringTest.java
+++ src/cn/edu/tsinghua/training/javase/basic/StringTest.java
@@ -8,7 +8,7 @@ public class StringTest {
 
     public static void main(String[] args) {
         String s = "hello";
-        String s1 = new String("hello");
+        String s1 = "hello";
 
         System.out.println(s);
         System.out.println(s1);
@@ -16,5 +16,21 @@ public class StringTest {
         System.out.println(s == s1);
 
         System.out.println(s.equals(s1));
+
+        System.out.println(s.indexOf('l'));
+
+        System.out.println(s.isEmpty());
+
+        System.out.println(s.matches("[A-z]+")); // regex regular expression 正则表达式
+
+        System.out.println(s.replace('l', 'a'));
+        System.out.println(s.replace("he", "ab"));
+        System.out.println(s.replaceAll("[A-z]+", "123"));
+
+        String s2 = "he ll o";
+        String[] strings = s2.split(" ");
+        for (String string : strings) {
+            System.out.println(string);
+        }
     }
 }

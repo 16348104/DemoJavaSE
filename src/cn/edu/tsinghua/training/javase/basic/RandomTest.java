7753afb6f0cf4ef4215931b6ccd6f9462888cff1
thu <mingfei.net@gmail.com>
2016-07-04 15:24:52 +0800

    Update.

--- src/cn/edu/tsinghua/training/javase/basic/RandomTest.java
+++ src/cn/edu/tsinghua/training/javase/basic/RandomTest.java
@@ -1,5 +1,8 @@
 package cn.edu.tsinghua.training.javase.basic;
 
+import java.util.Hashtable;
+import java.util.Random;
+
 /**
  * Created by Administrator
  * 2016/7/4.
@@ -8,4 +11,23 @@ package cn.edu.tsinghua.training.javase.basic;
 // 比较两种方法的效率
 // 比较两种方法的分布  Hashtable
 public class RandomTest {
+    public static void main(String[] args) {
+        Hashtable<Integer, Integer> hashtable = new Hashtable<>();
+        for (int i = 0; i < 20; i++) {
+            hashtable.put(i + 1, 0);
+        }
+        Random random = new Random();
+        long start = System.nanoTime();
+        for (int i = 0; i < 10000000; i++) {
+            int r = (int)(Math.random()*20) + 1;
+//            int r = random.nextInt(20) + 1;
+            hashtable.put(r, hashtable.get(r) + 1); // ******
+//            System.out.println(r);
+        }
+        System.out.println("total time: " + (System.nanoTime() - start));
+
+        for (Integer integer : hashtable.keySet()) {
+            System.out.println(integer + " > " + hashtable.get(integer));
+        }
+    }
 }

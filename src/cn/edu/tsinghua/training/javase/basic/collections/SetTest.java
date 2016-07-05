e008b2b86636f1e62ceaad9212a89691b6381056
thu <mingfei.net@gmail.com>
2016-07-05 13:51:39 +0800

    Update.

--- src/cn/edu/tsinghua/training/javase/basic/collections/SetTest.java
+++ src/cn/edu/tsinghua/training/javase/basic/collections/SetTest.java
@@ -1,7 +1,6 @@
 package cn.edu.tsinghua.training.javase.basic.collections;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created at 221
@@ -15,10 +14,45 @@ public class SetTest {
         integers.add(3);
 
 //        System.out.println(integers.);
-        System.out.println(integers.size());
+//        System.out.println(integers.size());
 
-        for (Integer integer : integers) {
-            System.out.print(integer);
+//        for (Integer integer : integers) {
+//            System.out.print(integer);
+//        }
+
+        Iterator<Integer> iterator = integers.iterator(); // 迭代器
+        while (iterator.hasNext()) {
+            Integer next = iterator.next();
+//            System.out.println(next);
+        }
+
+        Set<String> strings = new LinkedHashSet<>();
+        for (int i = 0; i < 100; i++) {
+            strings.add(i + "");
+        }
+
+        for (String string : strings) {
+//            System.out.println(string);
+        }
+
+        Set<Integer> set = new TreeSet<>();
+        set.add(0);
+        set.add(-1);
+        set.add(100);
+        set.add(-200);
+
+        for (Integer integer : set) {
+            System.out.println(integer);
+        }
+
+        Set<String> set1 = new TreeSet<>();
+        set1.add("asdf");
+        set1.add("apple");
+        set1.add("java");
+        set1.add("javaSe");
+
+        for (String s : set1) {
+            System.out.println(s);
         }
     }
 }

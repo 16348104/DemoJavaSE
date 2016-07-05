3382d118085fe6e8cd06667e799786c3ec96fe01
thu <mingfei.net@gmail.com>
2016-07-05 16:39:19 +0800

    Update.

--- src/cn/edu/tsinghua/training/javase/exercises/E19.java
+++ src/cn/edu/tsinghua/training/javase/exercises/E19.java
@@ -23,30 +23,32 @@ N 任意奇数
 */
 
 public class E19 {
+    private static final int N = 77;
+
     public static void main(String[] args) {
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < N / 2 + 1; i++) {
             // 1. space
-            for (int j = 3 - i; j > 0; j--) {
+            for (int j = (N / 2) - i; j > 0; j--) {
                 System.out.print(" ");
             }
             // 2. x
             for (int k = 0; k < i * 2; k++) {
-                System.out.print("x");
+                System.out.print("*");
             }
             // 3. x
-            System.out.println("x");
+            System.out.println("*");
         }
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < N / 2; i++) {
             // 1. space
             for (int j = 0; j < i + 1; j++) {
                 System.out.print(" ");
             }
             // 2. x
-            for (int k = 4 - i * 2; k > 0; k--) {
-                System.out.print("x");
+            for (int k = (N-3) - i * 2; k > 0; k--) {
+                System.out.print("*");
             }
             // 3. x
-            System.out.println("x");
+            System.out.println("*");
         }
 
     }

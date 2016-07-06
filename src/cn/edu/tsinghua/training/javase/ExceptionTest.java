72aff5cad6f9bbc6313386b2b98ee47bba7f2aa4
thu <mingfei.net@gmail.com>
2016-07-06 14:33:08 +0800

    Update.

--- src/cn/edu/tsinghua/training/javase/ExceptionTest.java
+++ src/cn/edu/tsinghua/training/javase/ExceptionTest.java
@@ -8,11 +8,15 @@ public class ExceptionTest {
     public static void main(String[] args) {
         int x = 1;
         int y = 0;
+        String[] strings = {};
+        String s = "hi";
         // ...
         try {
             System.out.println(x / y);
+            System.out.println(strings[0]);
+            System.out.println(s.charAt(2));
         } catch (ArithmeticException e) {
-        // ...
+            // ...
         }
 
         System.out.println("test...");

37e598af97ae4c03a3e200f4bec483564920e577
thu <mingfei.net@gmail.com>
2016-07-06 14:59:28 +0800

    Update.

--- src/cn/edu/tsinghua/training/javase/ExceptionTest.java
+++ src/cn/edu/tsinghua/training/javase/ExceptionTest.java
@@ -10,13 +10,23 @@ public class ExceptionTest {
         int y = 0;
         String[] strings = {};
         String s = "hi";
+        String string = "l23";
         // ...
         try {
+            int i = Integer.parseInt(string);
             System.out.println(x / y);
             System.out.println(strings[0]);
             System.out.println(s.charAt(2));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException...");
         } catch (ArithmeticException e) {
-            // ...
+            System.out.println("ArithmeticException...");
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("StringIndexOutOfBoundsException...");
+        } catch (NumberFormatException e) {
+            System.out.println("NumberFormatException...");
+        } finally {
+            System.out.println("finally...");
         }
 
         System.out.println("test...");

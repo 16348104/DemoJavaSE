2f5e4c207baaca64a55c0c58d3cf5736ebdc96ce
thu <mingfei.net@gmail.com>
2016-07-07 13:38:53 +0800

    Update.

--- src/cn/edu/tsinghua/training/javase/basic/InputStreamTest.java
+++ src/cn/edu/tsinghua/training/javase/basic/InputStreamTest.java
@@ -1,8 +1,6 @@
 package cn.edu.tsinghua.training.javase.basic;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 /**
  * Created at 221
@@ -10,11 +8,59 @@ import java.io.InputStream;
  */
 public class InputStreamTest {
     public static void main(String[] args) {
+//        InputStream inputStream = null;
+//        OutputStream outputStream = null;
+        Reader reader = null;
+        Writer writer = null;
         try {
-            InputStream inputStream = new FileInputStream("c:/test.txt");
-            System.out.println((char) inputStream.read());
+//            inputStream = new FileInputStream("c:/test.txt");
+//            outputStream = new FileOutputStream("c:/new.txt");
+//            int i = inputStream.read();
+//            while (i != -1) {
+//                System.out.print((char) i);
+//                outputStream.write(i);
+//                i = inputStream.read();
+//            }
+
+            reader = new FileReader("c:/test.txt");
+            writer = new FileWriter("c:/new.txt");
+            int i = reader.read();
+            while (i != -1) {
+                System.out.print((char)i);
+                writer.write(i);
+                i = reader.read();
+            }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+//            if (inputStream != null) {
+//                try {
+//                    inputStream.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//            if (outputStream != null) {
+//                try {
+//                    outputStream.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
